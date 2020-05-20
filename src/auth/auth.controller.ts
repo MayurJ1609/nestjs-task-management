424bdec67a7c62a8d92309ee995f3ade2e21b1ab
Mayur <mayur.jaganmohan@gmail.com>
2020-05-20 23:13:27 +0530

    Added auth controller, auth module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,38 @@
+import { User } from './user.entity';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './auth-user.decorator';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  signUp(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<void> {
+    return this.authService.signUp(authCredentialsDto);
+  }
+
+  @Post('/signin')
+  signIn(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.signIn(authCredentialsDto);
+  }
+
+  @Post('/test')
+  @UseGuards(AuthGuard()) //Understand AuthGaurd()
+  test(@GetUser() user: User) {
+    console.log('Controller User : ', user);
+  }
+}
