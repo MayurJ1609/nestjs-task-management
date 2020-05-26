28bfe2493d7ae7ccd1d9c5be43ee3a29362faf5c
Mayur <mayur.jaganmohan@gmail.com>
2020-05-26 22:01:21 +0530

    Removed unwanted logs

--- src/auth/auth-user.decorator.ts
+++ src/auth/auth-user.decorator.ts
@@ -4,8 +4,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const GetUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    console.log('Request : ', request);
-    console.log('User : ', request.User);
     return request.user;
   },
 );
